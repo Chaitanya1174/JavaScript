175e7dc053d78e824908988c545e7272709ad28d
Chaitanya1174 <124686870+Chaitanya1174@users.noreply.github.com>
2024-04-04 09:17:39 +0000

    05_Strings.js added

---
+++ Basics/05_Strings.js
@@ -0,0 +1,8 @@
+let var1= new String("Chaitanya")
+
+// You can use prototypes of this string using various prototype commands like .length, .touppercase(), etc.
+    console.log(var1); // op: [String: 'Chaitanya']
+    console.log(var1[2]); // op: a
+    console.log(var1.length); // op: 9
+    console.log(var1.toUpperCase()); // op: CHAITANYA
+    console.log(var1.indexOf('y')); // op: 7
