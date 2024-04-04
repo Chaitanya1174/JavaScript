7630649d2d4c34505ddd0912af639bfbd0155657
Chaitanya1174 <124686870+Chaitanya1174@users.noreply.github.com>
2024-04-04 11:50:46 +0000

    added 05

--- Basics/05_Strings.js
+++ Basics/05_Strings.js
@@ -6,3 +6,17 @@ let var1= new String("Chaitanya")
     console.log(var1.length); // op: 9
     console.log(var1.toUpperCase()); // op: CHAITANYA
     console.log(var1.indexOf('y')); // op: 7
+
+    const substring = var1.substring(4,9) //creating substring, negative values can't be used
+    console.log(substring); // op: tanya
+
+    const sliceSt = var1.slice(-4,9)
+    console.log(sliceSt); // op: anya
+
+    var2= "   chaitanya   "
+    const trim = var2.trim() //trims starting and ending spaces
+    console.log(trim);
+
+    console.log(var2.replaceAll('a','4'))
+
+    
