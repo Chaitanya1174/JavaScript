94e582f00124c20867083aca49e1c9b99d50a7a3
Chaitanya1174 <124686870+Chaitanya1174@users.noreply.github.com>
2024-04-05 08:01:50 +0000

    Basics 1 completed

---
+++ 01_Basics/07_Date.js
@@ -0,0 +1,13 @@
+// JS counts dates from jan 1 1970
+// ALWAYS USE FIRST LETTER CAPITAL IF YOU'RE USING INBUILT FUNCTION
+
+const myDate = new Date()
+console.log(myDate); // op: 2024-04-05T07:37:36.545Z
+console.log(myDate.toString()); // Op: Fri Apr 05 2024 07:37:36 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toLocaleString()); // op: 4/5/2024, 7:37:36 AM
+
+const createdDate = new Date(2004,6,11) // because js counts including 0
+console.log(createdDate.toDateString()); // op: Sun Jul 11 2004
+
+let inst = Date.now() // counts miliseconds since jan 1 1970
+console.log(inst); // op: 1712303105475
