522cb46435dd67ba5455052757f02391e8fa57b6
Chaitanya1174 <124686870+Chaitanya1174@users.noreply.github.com>
2024-04-08 06:52:03 +0000

    Basics completed

---
+++ 03_Basics/04_IIFE.js
@@ -0,0 +1,10 @@
+// immediately invoked function expressions 
+// only sounds complicated but isn't... it's just like declaring variable and using it inside a function at the same time
+function printHello() { // here, we named this fuction
+    console.log("hello");
+}
+printHello(); // here, we used that name to call this function
+
+(function printHola() {console.log("hola");})(); // iife doesn't know when to stop so use ; after line
+(()=>{console.log("life");})();// can also be used with arrow functions
+((name)=>{console.log(`Hii ${name}`);})("chaitanya");// use of parameters and arguments
