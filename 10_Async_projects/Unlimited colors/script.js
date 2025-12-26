b55ba2129c464f945eabd02227d359eb03a785b4
Chaitanya Chavan <chavanchaitanya1174@gmail.com>
2025-12-26 14:46:21 +0530

    Add async projects

---
+++ 10_Async_projects/Unlimited colors/script.js
@@ -0,0 +1,9 @@
+// Step 1: Import html elements
+// Step 2: Create function for random colors
+    // 2.1: Create an array of 0123456789ABCDEF
+    // 2.2: Add random index of array inside a variable using for loop
+    //      until 6 elements are added let's call it bgcolor
+// Step 3: Add event listener to start button
+    // 3.1: Use set interval to change background color to bgcolor
+// Step 4: Add event listener to stop button
+    // 4.1: Use clearInterval to stop changing color
